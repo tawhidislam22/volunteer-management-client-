93b329b6c299cb9d09ff80bf1057c80baa4aa234
tawhidislam22 <tawhidislam144@gmail.com>
2024-12-26 23:51:47 +0600

    recap done

--- src/Pages/VolunteerPostDetails/VolunteerPostDetails.jsx
+++ src/Pages/VolunteerPostDetails/VolunteerPostDetails.jsx
@@ -140,7 +140,7 @@ const VolunteerPostDetails = () => {
 
                                     <div className="mt-4 flex justify-between">
                                         <Link>
-                                        <button onClick={() => navigate(-1)}
+                                        <button 
                                             type="submit"
                                             className="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600 dark:bg-green-600 dark:hover:bg-green-700"
                                         >
